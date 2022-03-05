1202cacbf3e55aa65edb64a62a21b8a2486eafb0
Bui Tung Linh <linh.buitung18@gmail.com>
2022-03-05 16:28:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ movementConvertMobile(checkMobile);
 function movementConvertMobile(event) {
 	if (event.matches) {
 		setInterval(function () {
-			onCollideWall(160, -250, -280, 280);
+			onCollideWall(160, -250, -320, 320);
 		}, 1);
 		setInterval(function () {
 			onCollidePaddle(140, 145);
